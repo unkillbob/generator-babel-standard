be31ff3b7cb08ce52bfa419bcb3dd66b61ab7495
James Bunt <james.bunt@vendhq.com>
2015-04-30 20:35:39 +1200

    Fix unit tests. Fixes #5

--- test/test-app.js
+++ test/test-app.js
@@ -5,18 +5,29 @@ var path = require('path')
 var assert = require('yeoman-generator').assert
 var helpers = require('yeoman-generator').test
 
-describe('babel standard:app', function () {
+describe('babel-standard:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
+      .withPrompts({
+        moduleName: 'foo-bar',
+        githubUsername: 'fubar',
+        website: 'foobar.com'
+      })
       .on('end', done)
   })
 
-  it('creates files', function () {
+  it('generates expected files', function () {
     assert.file([
+      '.editorconfig',
+      '.gitattributes',
+      '.gitignore',
+      '.travis.yml',
+      'LICENSE',
+      'README.md',
       'package.json',
-      '.editorconfig'
+      'src/index.js',
+      'test/test.js'
     ])
   })
 })
